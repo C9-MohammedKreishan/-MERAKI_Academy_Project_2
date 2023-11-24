632958c3b99967bc50dea66a95a7ea185145e138
MohammedKreishan <mk.kreishan@gmail.com>
2023-11-25 00:43:44 +0300

    Project_2 << Book Store >>2

--- main.js
+++ main.js
@@ -31,7 +31,8 @@ const BOOKS = [
     title: "JavaScript from Beginner to Professional",
     author: " Laurence Lars Svekis",
     imageSrc: "./4.jpg",
-    description: "This book helps students get to grips with JavaScript fundamentals and apply them in code. It covers the components needed to write scripts, shows you how to apply JavaScript to create interactive web pages, and contains many exercises to get hands-on experience. <br><br>  Understanding the building blocks of JavaScript provides a foundation for your own applications, and the quick guides on HTML and JSON really make it the only book you’ll need when learning JavaScript for web development.",
+    description:
+      "This book helps students get to grips with JavaScript fundamentals and apply them in code. It covers the components needed to write scripts, shows you how to apply JavaScript to create interactive web pages, and contains many exercises to get hands-on experience. <br><br>  Understanding the building blocks of JavaScript provides a foundation for your own applications, and the quick guides on HTML and JSON really make it the only book you’ll need when learning JavaScript for web development.",
     rate: 8,
     price: 22,
   },
@@ -55,83 +56,100 @@ const BOOKS = [
   },
   // {},...
 ];
+Cart = [];
 
 const body = $("body");
 const logo = $(
-  '<header><img src="./Logo.jpg" id="logo"></img> <h3>Home</h3> <h3>Contact us</h3> <img src="./cart.png"></header>' 
+  '<header><img src="./Logo.jpg" id="logo"></img> <h3>Home</h3> <h3>Contact us</h3></header>'
 );
 const main = $('<div class= "books"></div>');
+main.appendTo(body);
 const footer = $("<footer>Home</footer>");
 const categories = $(
   "<div> Categories <select> <option>History</option><option>Anime</option></select></div>"
 );
+const CartImg = $('<img src="./cart.png" class=cart-img></img>');
 categories.appendTo(logo);
-
+CartImg.appendTo(logo);
+const BooksInCart = $(`<div class="Books-In-Cart"><div>`);
+const CartItems = $(`<div class="all-books-in-cart"></div>`);
+CartItems.appendTo(body);
 logo.appendTo(body);
 
-
-
+// Function shows a single book Title, image and description.
 const showSingleBook = (e) => {
   $(".books").hide();
-  console.log($(e).parent().html())
-  console.log($(e).parent().find("p").html());
-  let tempSrc = ""
-  let tempDiscription = ""
-
-BOOKS.forEach((element) => {
-  if (element.title === $(e).parent().find("h3").html()) {
-    console.log($(e).parent().find("h3").html());
-    tempSrc = element.imageSrc
-    tempDiscription = element.description
-  }
-})
+  let newItem = helpCart(e);
   const singleBook = $(
-    `<div class= "single-book"><div class="book-title"><h3> ${$(e).parent().find("h3").html()}</h3> <img src= "${tempSrc}"></img> <button class= "Add-to-cart">ADD To Cart</button></div><div class= "paragraphs"><p>${tempDiscription}</p> <p class= "rate">Mollitia cumque quos nemo dolore, excepturi a pariatur illum iure quod dolores?</p></div></div>`
+    `<div class= "single-book"><div class="book-title"><h3> ${newItem.title}</h3> <img src= "${newItem.imageSrc}"></img> <button class= "Add-to-cart">ADD To Cart</button></div><div class= "paragraphs"><p>${newItem.description}</p> <p class= "rate">Mollitia cumque quos nemo dolore, excepturi a pariatur illum iure quod dolores?</p></div></div>`
   );
-  $("Add-to-cart").on("click", () => {
-    Cart.push({ bookname: element.title});
-    console.log(Cart);
-    })
   singleBook.appendTo(body);
+
+  $("Add-to-cart").on("click", function () {
+    AddToCart(e);
+  });
 };
 
+// Add to Cart function
+const AddToCart = (e) => {
+  let newItem = helpCart(e);
+  const item = $(
+    `<div class="book-in-cart"> <h3>${newItem.title}</h3> <img src=${newItem.imageSrc}></img></div>`
+  );
+  item.appendTo(CartItems);
+  Cart.push(newItem);
 
-
-main.appendTo(body);
-Cart = [];
-
-const AddToCart = () => {
-  console.log($(".book>h3").html());
-  Cart.push($(".book>h3").html());
-
-  console.log(Cart);
 };
 
+// Show the Cart items (what we added to cart)
+
+const showCart = () => {
+  $(".books").hide();
+  $(".View-book").hide();
+  $(".all-books-in-cart").show();
+};
+
+//help function
+const helpCart = (e) => {
+  let tempObj = {};
+  BOOKS.forEach((element) => {
+    if (element.title === $(e).parent().find("h3").html()) {
+      tempObj.title = element.title;
+      tempObj.imageSrc = element.imageSrc;
+      tempObj.description = element.description;
+    }
+  });
+  return tempObj;
+};
+
+// Show all books function (Start function)
 const show_books = () => {
+
   BOOKS.forEach((element) => {
     const Book = $(
-      `<div class="book"> <h3>${
-        element.title
-      }</h3> <img src=${element.imageSrc}></img><button class="View-book">View</button></div>`
+      `<div class="book"> <h3>${element.title}</h3> <img src=${element.imageSrc}></img><button class="View-book">View</button></div>`
     );
     Book.appendTo(main);
-    const addfav = $(` <button class= "Add-to-cart">ADD TO Cart</button>`);
-    addfav.appendTo(Book);
-    addfav.on("click", () => {
-    Cart.push({ bookname: element.title});
-    console.log(Cart);
+    const addToCart = $(` <button class= "Add-to-cart">ADD TO Cart</button>`);
+    addToCart.appendTo(Book);
+    addToCart.on("click", function () {
+      AddToCart(this);
     });
   });
+  $(".all-books-in-cart").hide();
 
-  $(".View-book").on("click", function(){
-    // console.log(e.target)
-    showSingleBook(this)
+  $(".View-book").on("click", function () {
+    showSingleBook(this);
   });
   $("#logo").on("click", () => {
     $(".books").show();
     $(".single-book").hide();
+    $(".all-books-in-cart").hide();
+
   });
-  // $(".Add-to-cart").on("click", AddToCart);
+  $(CartImg).on("click", showCart);
+
+
   footer.appendTo(body);
 };
 show_books();
